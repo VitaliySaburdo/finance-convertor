dc3ee4e3d0211687e3354ef853934ed6db4fa421
VitaliySaburdo <vsaburdo@gmail.com>
2023-12-15 14:27:10 +0200

    finish

--- main.js
+++ main.js
@@ -1,27 +1,20 @@
 const url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
-
 const elementUSD = document.querySelector('[data-value="USD"]');
 const elementEUR = document.querySelector('[data-value="EUR"]');
 const elementGBP = document.querySelector('[data-value="GBP"]');
 
 const input = document.querySelector("#input");
 const result = document.querySelector("#result");
-const select_1 = document.querySelector("#select_1");
-const select_2 = document.querySelector("#select_2");
+const select = document.querySelector("#select");
 
-const rates = { UAH: 1 };
+const rates = {};
 
-for (let key in rates) {
-  const option = document.createElement("option");
-  option.textContent = key;
-  select_1.appendChild(option);
-}
-
-async function fetchData() {
+async function fetcData() {
   try {
     const response = await fetch(url);
     const data = await response.json();
     const result = await data;
+    console.log(result);
     rates.USD = result[24].rate;
     rates.EUR = result[31].rate;
     rates.GBP = result[23].rate;
@@ -34,8 +27,11 @@ async function fetchData() {
   elementGBP.textContent = rates.GBP.toFixed(2);
 }
 
-fetchData();
-setInterval(fetchData, 100000);
+fetcData();
+setInterval(fetcData, 100000);
+
+input.oninput = convertValue;
+select.oninput = convertValue;
 
 function convertValue() {
   result.value = (input.value / rates[select.value]).toFixed(2);
